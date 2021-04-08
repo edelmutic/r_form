e3f3558046c7e1d49690d12dd50d3004f3b83063
edelmutic <edelmutic@gmail.com>
2021-04-08 11:51:47 +0300

    ad hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,51 @@
-
 import './App.css';
-import React, { useState } from 'react';
+import content from './static'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { useForm, Controller } from 'react-hook-form';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 function App() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [emailDirty, setEmailDirty] = useState(false)
-  const [passwordDirty, setPasswordDirty] = useState(false)
-  const [emailError, setEmailError] = useState("Email can not be empty")
-  const [passwordError, setPasswordError] = useState("Password can not be empty")
 
-  const emailHandler = (e) => {
-    setEmail(e.target.value)
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(String(e.target.value).toLowerCase())) {
-      setEmailError('Incorrect email')
-    }
-    else {
-      setEmailError("")
-    }
-  }
+  const { control, handleSubmit, errors } = useForm()
 
-  const blurHandler = (e) => {
-    switch (e.target.name) {
-      case 'email':
-        setEmailDirty(true)
-        break
-      case 'password':
-        setPasswordDirty(true)
-        break
+  const classes = useStyles();
 
-    }
-  }
-
-  const passwordHandler = (e) => {
-    setPassword(e.target.value)
-    if (e.target.value.length < 3 || e.target.value.length > 8) {
-      setPasswordError('Password should be longer than 3 and shortet than 8')
-    } if (!e.target.value) {
-      setPasswordError("Password can not be empty")
-    }
-    else {
-      setPasswordError('')
-    }
-  }
-
+  const onSubmit = (data: ITextField) => console.log(data);
 
   return (
-    <div className="App">
-      <form className="form">
-        <h1>Registration</h1>
-        {(emailDirty && emailError) && <div style={{ color: "red" }}>{emailError}</div>}
-        <input onChange={e => emailHandler(e)} value={email} onBlur={e => blurHandler(e)} type="text" name="email" placeholder="Enter your email..." />
-        <br />
-        {(passwordDirty && passwordError) && <div style={{ color: "red" }}>{passwordError}</div>}
-        <input onChange={e => passwordHandler(e)} value={password} onBlur={e => blurHandler(e)} type="password" name="password" placeholder="Enter your password..." />
-        <br />
-        <button type="reset">Registration</button>
-
+    <div>
+      <form className={classes.root} onSubmit={handleSubmit(onSubmit)}>
+        {content.inputs.map((input, key) => {
+          return (
+            <div key={key}>
+              {/* <TextField id="standard-basic" label={input.label} type={input.type} /> */}
+              <Controller
+                name={input.label}
+                control={control}
+                defaultValue={input.type}
+                render={({ field }) => <input {...field} />}
+              />
+            </div>
+          )
+        })
+        }
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+      </Button>
       </form>
-
     </div>
-  );
+  )
 }
 
 export default App;
