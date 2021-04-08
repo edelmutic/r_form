fe0222c606659eb547005a03ea14acedcb1bfd03
edelmutic <edelmutic@gmail.com>
2021-04-08 18:34:46 +0300

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,37 @@
-import './App.css';
-import React from 'react';
+import React from "react";
+import './App.css'
 import { useForm } from "react-hook-form";
+import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
-import content from './static';
+
+const schema = yup.object().shape({
+  username: yup.string().required(),
+  password: yup.string().required().min(6),
+})
 
 
-function App() {
-
-  const schema = Yup.object().shape({
-
-    userName: Yup.string().required("username is required"),
-    password: Yup.string().required('Password is required').min(6)
-  })
-
-  const { register, handleSubmit } = useForm({
+export default function App() {
+  const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(schema)
-  });
 
-  const onSubmit = (data) => console.log(data)
+  });
+  const onSubmit = (data) => console.log(data);
+
 
   return (
     <div className="App">
       <h1>React hook form</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {content.inputs.map((input, key) => {
-          return (
-            <div key={key}>
-              <p>
-                <label>{input.label}</label>
-              </p>
-              <p>
-                <input name={input.name} className="input" type={input.type} />
-              </p>
-            </div>
-          )
-        })}
+      <form onSubmit={handleSubmit(onSubmitnpm)}>
+        <p><label>Username</label></p>
+        <p>
+          <input className="input" {...register("userName")} />
+        </p>
+        <p><label>Password</label></p>
+        <p>
+          <input className="input" {...register("Password")} />
+        </p>
         <button className="btn" type="submit">Submit</button>
       </form>
     </div>
-  )
+  );
 }
-
-export default App;
-
