e5b2f17b30ab04a299925ae31fe82cb33ca75ec3
edelmutic <edelmutic@gmail.com>
2021-04-08 19:26:49 +0300

    add validation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,18 @@ import './App.css'
 import { useForm } from "react-hook-form";
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 const schema = yup.object().shape({
   username: yup.string().required(),
@@ -11,26 +23,32 @@ const schema = yup.object().shape({
 
 
 export default function App() {
-  const { register, handleSubmit, errors } = useForm({
-    resolver: yupResolver(schema)
+  const classes = useStyles();
+
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+
 
   });
-  const onSubmit = (data) => console.log(data);
+
+
+  const onSubmit = (data) => alert(data)
+
+
 
 
   return (
     <div className="App">
       <h1>React hook form</h1>
-      <form onSubmit={handleSubmit(onSubmitnpm)}>
-        <p><label>Username</label></p>
-        <p>
-          <input className="input" {...register("userName")} />
-        </p>
-        <p><label>Password</label></p>
-        <p>
-          <input className="input" {...register("Password")} />
-        </p>
-        <button className="btn" type="submit">Submit</button>
+
+      <form onSubmit={handleSubmit(onSubmit)} className={classes.root}>
+
+        <TextField id="standard-basic" label="Username" {...register("username")} />
+        <br />
+
+        <TextField id="standard-basic" label="Password" {...register("password")} />
+        <br />
+        <Button type="submit" variant="contained" color="primary">Submit</Button>
       </form>
     </div>
   );
